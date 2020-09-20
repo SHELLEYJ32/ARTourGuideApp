e8d02ac9fd417773a9defca3c44d6bde5d235bc8
Shan Jiang <jiang25s@mtholyoke.edu>
2020-09-19 22:41:28 -0400

    updated basic functions

---
+++ Assets/Scenes/ARTourGuide/RefreshSession.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Reloads the ARSession by first destroying the ARSession's GameObject
+/// and then instantiating a new ARSession from a Prefab.
+/// </summary>
+public class RefreshSession : MonoBehaviour
+{
+    public ARSession session;
+    public GameObject sessionPrefab;
+
+    public void ReloadSession()
+    {
+        if (session != null)
+        {
+            StartCoroutine(DoReload());
+        }
+    }
+
+    IEnumerator DoReload()
+    {
+        Destroy(session.gameObject);
+        yield return null;
+
+        if (sessionPrefab != null)
+        {
+            session = Instantiate(sessionPrefab).GetComponent<ARSession>();
+        }
+
+    }
+}
