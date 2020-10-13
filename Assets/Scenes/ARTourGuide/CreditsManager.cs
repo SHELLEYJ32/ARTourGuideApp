d97fc4e76319e5b9800acf70d9a68d4aba4f8be4
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2020-10-12 20:19:57 -0400

    added credits

---
+++ Assets/Scenes/ARTourGuide/CreditsManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsManager : MonoBehaviour
+{
+    public GameObject creditsPanel;
+
+    public void ShowOrHideCredits()
+    {
+        if (creditsPanel.activeSelf)
+            creditsPanel.SetActive(false);
+        else
+            creditsPanel.SetActive(true);
+    }
+}
