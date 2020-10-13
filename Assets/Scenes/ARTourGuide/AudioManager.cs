f801d1d8d589686f3f43b261123870eed982b826
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2020-10-12 21:09:55 -0400

    updated menu buttons and plane prefab

---
+++ Assets/Scenes/ARTourGuide/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sprite playImage;
+    public Sprite pauseImage;
+    public Button playPauseButton;
+
+    public void PlayOrPause()
+    {
+        if (playPauseButton.image.sprite == pauseImage)
+        {
+            playPauseButton.image.sprite = playImage;
+            GetComponent<AudioSource>().Pause();
+        }
+        else
+        {
+            playPauseButton.image.sprite = pauseImage;
+            GetComponent<AudioSource>().UnPause();
+        }
+    }
+}
